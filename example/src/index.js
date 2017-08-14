7a61ce23c6e07bf9189ff9af17331bdbfb6f7333
Erik Poort <erik@mediamonks.com>
2017-08-14 18:38:33 -0300

    Start on setting up example

--- example/src/index.js
+++ example/src/index.js
@@ -1,28 +1,10 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SingleView } from 'react-native-native-navigation';
 
 export default class example extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native Native Navigation!
-        </Text>
-      </View>
+      <SingleView />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 15,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+const SingleView = class extends Component {
+	render() {
+		return <Text>Hoi</Text>
+	}
+}
+
+export default { SingleView };

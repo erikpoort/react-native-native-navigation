8d886d64f793508c2341311e051544a4b7eef4c2
Erik Poort <erik@mediamonks.com>
2017-10-21 20:57:43 -0300

    Add validation for add node to SingleView screen

--- example/src/Detail/index.js
+++ example/src/Detail/index.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
 import { TabView, SingleView } from 'react-native-native-navigation';
 import Home from './../Home';
-import Detail2 from './../Detail2';
+import Detail2 from './../Detail';
 
 export default class Detail extends Component {
 	handleDetail = () => {
 		this.props.single.showModal(<TabView id="stack">
 			<SingleView id="Home" screen={Home}/>
-			<Detail2 />
+			<SingleView id="Detail 2" screen={Detail2}/>
 		</TabView>);
 	};
 	render(){

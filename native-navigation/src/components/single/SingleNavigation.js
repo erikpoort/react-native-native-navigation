767a1b95d0e8d7b8e838c554abc10115de3d4309
Juan Curti <juancurti22@gmail.com>
2017-12-27 13:49:21 -0300

    BaseNavigation class

--- native-navigation/src/components/single/SingleNavigation.js
+++ native-navigation/src/components/single/SingleNavigation.js
@@ -5,24 +5,17 @@ const { ReactNativeNativeNavigation } = NativeModules;
 import { Navigation } from './../Navigation';
 import StackView from './../stack/StackView';
 import SingleView from './../single/SingleView';
+import BaseNavigation from '../BaseNavigation';
 
-export default class SingleNavigation {
+export default class SingleNavigation extends BaseNavigation{
 	screenID;
 
 	constructor(screenID) {
+		super()
 		this.screenID = screenID;
 	}
 
 	showModal = (showScreen) => {
-		const Screen = showScreen;
-		const screenData = Navigation.mapChild(Screen, `${this.screenID}/modal`);
-
-		return ReactNativeNativeNavigation.showModal(screenData, (register) => {
-			const view = Navigation.viewMap[register.type];
-			const registerScreens = view.reduceScreens(register, Navigation.viewMap, Navigation.pageMap).filter((screen) => {
-				return screen.screenID.includes(this.screenID) && screen.screenID !== this.screenID;
-			});
-			Navigation.registerScreens(registerScreens);
-		});
+		return super.registerScreens(this, Navigation, showScreen);
 	}
 }

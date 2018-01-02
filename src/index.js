5fab90e1babbb8b8b2e9822eff6c5d64d3ad5349
Juan Curti <juancurti22@gmail.com>
2018-01-02 10:07:57 -0300

    Changed "store" prop to "customStore".

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ import store from './store';
 export default class example extends Component {
 	render() {
 		return (
-			<Navigation pages={[Loading, Home]} provider={Provider} store={store}>
+			<Navigation pages={[Loading, Home]} provider={Provider} customStore={store}>
 				<Loading/>
 				<StackView id='stack'>
 					<SingleView id='home' screen={Home} lazyLoad='true'/>

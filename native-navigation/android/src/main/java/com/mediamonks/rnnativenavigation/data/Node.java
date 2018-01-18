9ce42c9b440400a02ca66e751cac508a5c92b1c3
Leonardo Acevedo <leonardo.acevedo@mediamonks.com>
2018-01-18 14:34:20 -0300

    Android: remove Serializable inheritance from interface Node, because it is not being used.

--- native-navigation/android/src/main/java/com/mediamonks/rnnativenavigation/data/Node.java
+++ native-navigation/android/src/main/java/com/mediamonks/rnnativenavigation/data/Node.java
@@ -5,14 +5,12 @@ import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableMap;
 import com.mediamonks.rnnativenavigation.factory.BaseFragment;
 
-import java.io.Serializable;
-
 /**
  * Created by erik on 09/08/2017.
  * RNNativeNavigation 2017
  */
 
-public interface Node extends Serializable {
+public interface Node {
     void setInstanceManager(ReactInstanceManager instanceManager);
 
     void setData(ReadableMap map);

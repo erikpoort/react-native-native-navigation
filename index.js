2af4d7c154ab28f60362355cfa76a625f37cc4ab
Leonardo Acevedo <leonardo.acevedo@mediamonks.com>
2018-03-13 12:49:33 -0300

    Try to make standalone library

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import { AppRegistry } from 'react-native';
-import App from './src';
-
-AppRegistry.registerComponent('example', () => App);
+export import * from './src';
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import {
+export import {
 	Navigation,
 	DrawerView,
 	SingleView,
@@ -7,42 +6,3 @@ import {
 	StackView,
 	TabView
 } from '../native-navigation';
-import Home from './pages/Home';
-import Menu from './pages/Menu';
-import Detail from './pages/Detail'
-import Loading from './pages/Loading';
-
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import ExampleView, { ExampleNode } from './custom_nodes/ExampleView';
-
-export default class example extends Component {
-	constructor() {
-		super();
-
-		this.home = new Home();
-		const navigation = new Navigation(
-			[Home, Menu, Detail],
-			[ExampleNode],
-			Provider, store
-		);
-
-		navigation.start(
-			<DrawerView
-				id='drawer'
-				left={
-					<SplitView id='split' axis={SplitView.AXIS.VERTICAL}>
-						<SingleView id='menu' screen={Menu}/>
-						<StackView id='stack'>
-							<SingleView id='detail' screen={Detail}/>
-						</StackView>
-					</SplitView>
-				}
-				center={<ExampleView id='menu' screen={Menu}/>}/>
-		);
-	}
-
-	render() {
-		return <Loading/>
-	}
-}

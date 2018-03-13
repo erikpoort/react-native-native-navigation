5b5d295a5e0d47f78e4ab362854db69a3e13cd55
Erik Poort <erik@mediamonks.com>
2018-03-13 12:37:07 -0300

    Add base split navigation

---
+++ native-navigation/src/components/split/SplitNavigation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { NativeModules } from 'react-native';
+import BaseNavigation from '../BaseNavigation';
+
+const { ReactNativeNativeNavigation } = NativeModules;
+
+export default class SplitNavigation extends BaseNavigation {
+
+}
